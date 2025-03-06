010adb94f13b003a90e132354d7ae1880f6bcede
fabuzard <44804900+fabuzard@users.noreply.github.com>
2025-03-06 13:00:27 +0700

    Add env

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,5 @@
-const API_KEY = "ec6ab290";
-const BASE_URL = "https://www.omdbapi.com/";
-
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL; 
 export async function searchMovies(title) {
   const url = `${BASE_URL}?apikey=${API_KEY}&s=${title}`;
 
